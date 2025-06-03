72cf149e210c26016f488f67339cf8cf3c3b7c3e
Julia-Mamaeva <julia0601alex@icloud.com>
2025-06-03 14:20:08 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,21 +8,21 @@
 class PMTree {
  public:
     explicit PMTree(const std::vector<char>& elements);
-    
+
     struct Node {
         char value;
         std::vector<std::shared_ptr<Node>> children;
-        
+
         explicit Node(char val) : value(val) {}
     };
-    
+
     std::shared_ptr<Node> getRoot() const { return root_; }
     size_t getPermutationsCount() const { return total_permutations_; }
 
  private:
     std::shared_ptr<Node> root_;
     size_t total_permutations_;
-    
+
     void buildTree(std::shared_ptr<Node> parent,
                  const std::vector<char>& remaining_elements);
 };
